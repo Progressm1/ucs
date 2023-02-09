30993461833d9099ec3ae1c58b207b65cefe5ae8
Progressm1 <122403364+Progressm1@users.noreply.github.com>
2023-02-10 00:13:17 +0530

    Update ucs.py

--- ucs.py
+++ ucs.py
@@ -3,26 +3,26 @@ class PQueue():
         self.dict = {}
         self.keys = []
         self.sorted = False
-        
+    
     def push(self, k, v):
         self.dict[k] = v
         self.sorted = False
-        
+    
     def _sort(self):
-        self.keys = sorted(self.dict, key=self.dict.get)
+        self.keys = sorted(self.dict, key=self.dict.get, reverse=True)
         self.sorted = True
-        
+    
     def pop(self):
         try:
             if not self.sorted:
                 self._sort()
-            key = self.keys.pop(0)
+            key = self.keys.pop()
             value = self.dict[key]
             self.dict.pop(key)
             return key, value
         except:
             return None
-        
+
 def path_costs(path):
     c = {}
     with open(path, 'r') as file:
@@ -42,15 +42,15 @@ def ucs(start, goal, g):
     frontier = PQueue()
     # pushing path and cost to pqueue
     frontier.push(start, 0)
+
     while True:
         # poping path with least cost
         path, cost = frontier.pop()
-        if path is None:
-            return None
+        print(path + " " + str(cost))
         # splitting out end node in path
         end = path.split("->")[-1]
         if goal == end:
-            return path, cost
+            break
         for node, weight in g[end].items():
             # adding edge weight(cost) to total cost
             new_cost = cost + weight
@@ -58,12 +58,4 @@ def ucs(start, goal, g):
             # adding new path and cost to pqueue
             frontier.push(new_path, new_cost)
 
-graph = path_costs('/home/Paths.txt')
-start = 'Arad'
-goal = 'Bucharest'
-path, cost = ucs(start, goal, graph)
-if path is not None:
-    print("Path: ", path)
-    print("Cost: ", cost)
-else:
-    print("No path found from {} to {}".format(start, goal))
+ucs('Arad', 'Bucharest', path_costs('/home/Paths.txt'))
